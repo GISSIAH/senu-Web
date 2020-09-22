8f948588dcbb140a114e0d0d43c4bff4eec89bd5
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2020-09-22 21:32:40 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,27 @@
 const router = require('express').Router();
 const axios = require('axios');
-router.get('/',(req,res)=>{
+const bodyparser = require('body-parser')
+
+const urlenco = bodyparser.urlencoded({extended:true})
+
+
+router.get('/vis',(req,res)=>{
     console.log('home');
     res.render('home')
+});
+
+router.get('/entry',(req,res)=>{
+    console.log('entry')
+    res.render('entry')
+});
+
+router.post('/entry',urlenco,(req,res)=>{
+    axios.post('http://localhost:4000/api/',req.body).then(data=>{
+        console.log(data)
+    }).catch(err=>{
+        console.log(err)
+    })
+    res.send('Succesfully')
 });
 
 router.post('/',(req,res)=>{
