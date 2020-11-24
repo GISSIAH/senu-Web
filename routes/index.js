549c2c794fb5d9e48b8c9f1d6c09e90620b19d52
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2020-11-23 22:57:13 -0800

    formatted

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,48 @@
 const router = require('express').Router();
 const axios = require('axios');
 const bodyparser = require('body-parser')
-const urlenco = bodyparser.urlencoded({extended:true})
-
-router.get('/',(req,res)=>{
+const urlenco = bodyparser.urlencoded({
+    extended: true
+})
+router.get('/', (req, res) => {
     res.send('HOME PLACE')
 })
-router.get('/vis',(req,res)=>{
-    axios.get('https://senu-back.herokuapp.com/uptime').then(function(data){
-    res.render('vis',{time:data.data.Latest})
-    }).catch(err=>{
+router.get('/vis', (req, res) => {
+    axios.get('https://senu-back.herokuapp.com/uptime').then(function (data) {
+        res.render('vis', {
+            time: data.data.Latest
+        })
+    }).catch(err => {
         console.log(err)
     })
 });
-router.get('/test',(req,res)=>{
+router.get('/test', (req, res) => {
     console.log('test');
     res.render('test')
 });
-router.get('/entry',(req,res)=>{
+router.get('/entry', (req, res) => {
     console.log('entry nig')
     res.render('record')
 });
-router.post('/entry',urlenco,(req,res)=>{
-    var new_body={'name':req.body.name,
-        'type':req.body.type,
-        'admitted':req.body.admitted,
-        'doctors':req.body.doctors,
-        'nurses':req.body.nurses,
-        'time':Date()}
+router.post('/entry', urlenco, (req, res) => {
+    var new_body = {
+        'name': req.body.name,
+        'type': req.body.type,
+        'admitted': req.body.admitted,
+        'doctors': req.body.doctors,
+        'nurses': req.body.nurses,
+        'time': Date()
+    }
 
-    axios.post('https://senu-back.herokuapp.com/',new_body).then(data=>{
+    axios.post('https://senu-back.herokuapp.com/', new_body).then(data => {
         console.log(data)
-    }).catch(err=>{
+    }).catch(err => {
         console.log(err)
     })
     res.render('record')
 });
-router.post('/',(req,res)=>{
+router.post('/', (req, res) => {
     console.log()
-    
+
 })
 module.exports = router;
